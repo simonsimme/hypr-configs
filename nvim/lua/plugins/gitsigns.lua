be37c45f1a0c0a9b23efe6523d56af48474fd404
simonsimme <simonsimme@hotmail.se>
2025-10-28 22:32:22 +0100

    git to nvim

--- nvim/lua/plugins/gitsigns.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"lewis6991/gitsigns.nvim",
-	config = function()
-		require("gitsigns").setup()
-
-    vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-    vim.keymap.set("n", "<leader>gs", ":Gitsigns<CR>", {})
-	end,
-}
